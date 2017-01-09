0083085793ff3850637804adfcad13888d633893
Ashimar <996606326@qq.com>
2017-01-09 18:12:05 +0800

    change MD

---
+++ js/view/SegmentedView.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from 'react-native';
+
+export default class SegmentedView extends Component {
+  constructor() {
+    super()
+  }
+
+  render (){
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create ({
+  constainer: {
+    margin: 10,
+    backgroundColor: '#9e9e9e',
+    borderColor: '#9e9e9e',
+    borderWidth: 0.5,
+    borderRadius: 10,
+  }
+});
